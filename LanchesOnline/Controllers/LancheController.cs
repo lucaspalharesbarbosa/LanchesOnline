9e0f3ef989837ee2b822397112120a7bd17d196e
Lucas <lucasp.b@hotmail.com>
2020-08-05 21:37:46 -0300

    - Criado o 'LancheController'. - Criada a view 'List' para listar todos os lanches.

---
+++ LanchesOnline/Controllers/LancheController.cs
@@ -0,0 +1,23 @@
+﻿using LanchesOnline.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesOnline.Controllers {
+    public class LancheController : Controller {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaLancheRepository _cagegoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaLancheRepository cagegoriaRepository) {
+            _lancheRepository = lancheRepository;
+            _cagegoriaRepository = cagegoriaRepository;
+        }
+
+        public IActionResult List() {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categoria";
+
+            var lanches = _lancheRepository.Lanches;
+
+            return View(lanches);
+        }
+    }
+}
