9e6eeebaec62fbab745c09310751eb33e2c64640
Lucas <lucasp.b@hotmail.com>
2020-08-05 22:09:26 -0300

    - Alterado para exibir os lanches preferidos na home.

--- LanchesOnline/Controllers/HomeController.cs
+++ LanchesOnline/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanchesOnline.Repositories.Interfaces;
+using LanchesOnline.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace LanchesOnline.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        private readonly ILancheRepository _lancheRepository;
 
-        public HomeController(ILogger<HomeController> logger) {
+        public HomeController(ILogger<HomeController> logger, ILancheRepository lancheRepository) {
             _logger = logger;
+            _lancheRepository = lancheRepository;
         }
 
         public IActionResult Index() {
-            return View();
+            return View(new HomeViewModel {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            }); ;
         }
 
         public IActionResult Privacy() {
---
+++ LanchesOnline/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using LanchesOnline.Models;
+using System.Collections.Generic;
+
+namespace LanchesOnline.ViewModels {
+    public class HomeViewModel {
+        public IEnumerable<Lanche> LanchesPreferidos{ get; set; }
+    }
+}
