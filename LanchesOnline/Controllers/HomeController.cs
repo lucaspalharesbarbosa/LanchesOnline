3c3087e5c3641cb1474f8fe8a0614670db1724e7
Lucas <lucasp.b@hotmail.com>
2020-08-10 22:33:03 -0300

    - Implementada o carousel da view index da página principal.

--- LanchesOnline/Controllers/HomeController.cs
+++ LanchesOnline/Controllers/HomeController.cs
@@ -1,26 +1,19 @@
 ﻿using LanchesOnline.Repositories.Interfaces;
 using LanchesOnline.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace LanchesOnline.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
         private readonly ILancheRepository _lancheRepository;
 
-        public HomeController(ILogger<HomeController> logger, ILancheRepository lancheRepository) {
-            _logger = logger;
+        public HomeController(ILancheRepository lancheRepository) {
             _lancheRepository = lancheRepository;
         }
 
         public IActionResult Index() {
             return View(new HomeViewModel {
                 LanchesPreferidos = _lancheRepository.LanchesPreferidos
-            }); ;
-        }
-
-        public IActionResult Privacy() {
-            return View();
+            });
         }
     }
 }
